4120120bfaade2fde986fa5406beecd84c1597cf
kevin <contact@whatsnextforkev.in>
2026-03-10 19:56:33 -0700

    adds Sanity.io CMS (#1)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
 // @ts-check
+import 'dotenv/config';
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+
+import sanity from '@sanity/astro';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [sanity({
+      projectId: process.env.SANITY_PROJECT_ID,
+      dataset: process.env.SANITY_DATASET,
+      useCdn: false, // for static builds
+    }),
+  ],
 });
